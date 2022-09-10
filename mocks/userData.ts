acfd517024250b52ae674eb5160f90ad7bb0eb53
kevscript <kevscripted@gmail.com>
2022-09-10 02:33:59 +0200

    updated userData mocks

--- mocks/userData.ts
+++ mocks/userData.ts
@@ -1,6 +1,11 @@
-import { collections } from "./collections";
+import { Collection, collections } from "./collections";
 
-export const userData = {
+export type UserData = {
+  userId: string;
+  collections: Collection[];
+};
+
+export const userData: UserData = {
   userId: "1",
-  collections: [...collections]
-}
+  collections: [...collections],
+};
