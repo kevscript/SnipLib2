9d80c82231db82ed623af368057eb387a35dae5e
kevscript <kevscripted@gmail.com>
2022-09-15 16:40:05 +0200

    added userData initialization on first user auth + created api route to get userData

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,7 +2,8 @@ import NextAuth, { NextAuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 import clientPromise from "../../../lib/mongodb";
-import { v4 as uuidv4 } from "uuid";
+import { ObjectID } from "bson";
+import { UserData } from "@/mocks/userData";
 
 export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
@@ -21,10 +22,45 @@ export const authOptions: NextAuthOptions = {
     async signIn({ user, account, profile }) {
       return true;
     },
-    async jwt({ token, user }) {
+    async jwt({ token, user, isNewUser }) {
       if (user) {
         token.id = user.id;
+
+        if (isNewUser) {
+          const m = await clientPromise;
+
+          const initUserData: UserData = {
+            userId: user.id,
+            collections: [
+              {
+                _id: new ObjectID(),
+                label: "sandbox",
+                snippets: [
+                  {
+                    _id: new ObjectID(),
+                    title: "Hello World example",
+                    language: "html",
+                    description: "My first snippet on SnipLib.",
+                    content: "<h1>Hello World</h1>",
+                    createdAt: Date.now(),
+                    updatedAt: Date.now(),
+                    favorite: false,
+                    public: false,
+                    tags: ["hello", "world"],
+                  },
+                ],
+              },
+            ],
+          };
+
+          const createdUserData = await m
+            .db("sniplib")
+            .collection("usersData")
+            .insertOne(initUserData);
+          console.log("data created", createdUserData);
+        }
       }
+
       return token;
     },
   },
