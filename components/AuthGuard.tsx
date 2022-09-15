0d73179cc6619e88ff3320d53e4f9fac9a7dcc04
kevscript <kevscripted@gmail.com>
2022-09-15 16:47:21 +0200

    created AuthGuard and Layout components

---
+++ components/AuthGuard.tsx
@@ -0,0 +1,28 @@
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import { ReactNode, useEffect, useState } from "react";
+
+type AuthGuardProps = {
+  children: ReactNode;
+};
+
+const AuthGuard = ({ children }: AuthGuardProps) => {
+  const router = useRouter();
+  const { data: session, status } = useSession();
+  const isAuth = session && status === "authenticated" ? true : false;
+
+  useEffect(() => {
+    if (status === "loading") return;
+    if (!isAuth) router.push("/");
+  }, [isAuth, status, router]);
+
+  if (isAuth) return <>{children}</>;
+
+  return (
+    <div className="flex items-center justify-center w-full h-screen">
+      <h1>Loading...</h1>
+    </div>
+  );
+};
+
+export default AuthGuard;
