ce5d503f623e2729fe733a3d31e7bb6fe79d06d2
kevscript <kevscripted@gmail.com>
2022-09-20 21:56:11 +0200

    updated useData with route handlers

--- hooks/useData.tsx
+++ hooks/useData.tsx
@@ -1,28 +1,117 @@
 import { useQuery } from "@tanstack/react-query";
+import { CollectionType } from "models/Collection";
+import { SnippetType } from "models/Snippet";
 import { UserDataType } from "models/UserData";
 import { useSession } from "next-auth/react";
-import { createContext, useContext } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 export type UserDataProviderReturnValue = {
   isLoading: boolean;
   error: any;
-  data: UserDataType;
+  collections: CollectionType[] | undefined;
+  snippets: SnippetType[] | undefined;
+  activeCollectionId: string;
+  activeSnippetId: string;
+  checkCollection: (collectionId: string) => void;
+  checkSnippet: ({
+    snippetId,
+    collectionId,
+  }: {
+    snippetId: string;
+    collectionId: string;
+  }) => void;
 };
 
 export const fetchUserData = async () => {
   const res = await fetch("/api/userData");
-  const data = await res.json();
+  const data: UserDataType = await res.json();
   return data;
 };
 
 export const useDataProvider = () => {
   const { status } = useSession();
 
-  const { data, error, isLoading } = useQuery(["userData"], fetchUserData, {
+  const {
+    data: { collections, snippets } = {},
+    error,
+    isLoading,
+  } = useQuery(["userData"], fetchUserData, {
     enabled: status === "authenticated",
+    onSuccess: (data) => initApp(data),
   });
 
-  return { data, error, isLoading };
+  const [activeCollectionId, setActiveCollectionId] = useState("");
+  const [activeTagLabel, setActiveTagLabel] = useState("");
+  const [activeSnippetId, setActiveSnippetId] = useState("");
+
+  const initApp = (data: UserDataType) => {
+    const defaultCollection = data.collections?.find((c) => c.default);
+
+    if (defaultCollection) {
+      const firstSnippet = data.snippets.find(
+        (s: any) => s.collectionId === defaultCollection._id
+      );
+      firstSnippet && setActiveSnippetId(firstSnippet._id.toString());
+      setActiveCollectionId(defaultCollection._id.toString());
+    }
+  };
+
+  const checkCollection = (collectionId: string) => {
+    if (collections) {
+      const existingCollection = collections.find(
+        (c) => c._id === collectionId
+      );
+      if (existingCollection && snippets) {
+        const firstSnippet = snippets.find(
+          (s) => s.collectionId === existingCollection._id
+        );
+        firstSnippet
+          ? setActiveSnippetId(firstSnippet._id.toString())
+          : activeSnippetId && setActiveSnippetId("");
+        setActiveCollectionId(existingCollection._id.toString());
+      } else {
+        activeCollectionId && setActiveCollectionId("");
+      }
+    }
+  };
+
+  const checkSnippet = ({
+    snippetId,
+    collectionId,
+  }: {
+    snippetId: string;
+    collectionId: string;
+  }) => {
+    if (collections) {
+      const existingCollection = collections.find(
+        (c) => c._id === collectionId
+      );
+
+      if (existingCollection && snippets) {
+        const existingSnippet = snippets.find(
+          (s) => s._id === snippetId && s.collectionId === collectionId
+        );
+
+        existingSnippet
+          ? setActiveSnippetId(existingSnippet._id.toString())
+          : activeSnippetId && setActiveSnippetId("");
+        setActiveCollectionId(existingCollection._id.toString());
+      } else {
+        activeCollectionId && setActiveCollectionId("");
+      }
+    }
+  };
+
+  return {
+    collections,
+    snippets,
+    error,
+    isLoading,
+    activeCollectionId,
+    activeSnippetId,
+    checkCollection,
+    checkSnippet,
+  };
 };
 
 export const dataContext = createContext({} as UserDataProviderReturnValue);
