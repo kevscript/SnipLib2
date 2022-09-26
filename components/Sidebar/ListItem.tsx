27b93acb944dd4667faac585de5f69e53355cbb1
kevscript <kevscripted@gmail.com>
2022-09-26 17:53:10 +0200

    updated Sidebar items with conditional Links

--- components/Sidebar/ListItem.tsx
+++ components/Sidebar/ListItem.tsx
@@ -16,6 +16,32 @@ const ListItem = ({
   active,
   listId,
 }: ListItemProps) => {
+  if (active) {
+    return (
+      <li className="relative flex justify-between flex-shrink-0 h-10 text-sm cursor-pointer flex-nowrap group">
+        <div className="flex flex-nowrap">
+          <Branch short={first} active={active} />
+          <span
+            className={`ml-4 ${
+              active ? "text-white" : "text-carbon-300"
+            } group-hover:text-white`}
+          >
+            {label}
+          </span>
+        </div>
+        <div className="flex justify-center w-6">
+          <span
+            className={`${
+              active ? "text-white" : "text-carbon-300"
+            } group-hover:text-white`}
+          >
+            {amount}
+          </span>
+        </div>
+      </li>
+    );
+  }
+
   return (
     <Link href={`/lists/${listId}`} passHref>
       <li className="relative flex justify-between flex-shrink-0 h-10 text-sm cursor-pointer flex-nowrap group">
--- components/Sidebar/TagItem.tsx
+++ components/Sidebar/TagItem.tsx
@@ -9,6 +9,32 @@ export type TagItemProps = {
 };
 
 const TagItem = ({ first, active, label, amount }: TagItemProps) => {
+  if (active) {
+    return (
+      <li className="relative flex justify-between flex-shrink-0 h-10 text-sm cursor-pointer flex-nowrap group">
+        <div className="flex flex-nowrap">
+          <Branch short={first} active={active} />
+          <span
+            className={`ml-4 ${
+              active ? "text-white" : "text-carbon-300"
+            } group-hover:text-white`}
+          >
+            {"#" + label}
+          </span>
+        </div>
+        <div className="flex justify-center w-6">
+          <span
+            className={`${
+              active ? "text-white" : "text-carbon-300"
+            } group-hover:text-white`}
+          >
+            {amount}
+          </span>
+        </div>
+      </li>
+    );
+  }
+
   return (
     <Link href={`/tags/${label}`} passHref>
       <li className="relative flex justify-between flex-shrink-0 h-10 text-sm cursor-pointer flex-nowrap group">
