edad8e86671f5070ee24f961be37e49791850c87
kevscript <kevscripted@gmail.com>
2022-09-28 18:40:25 +0200

    added snippet page handling

--- pages/snippets/index.tsx
+++ pages/snippets/index.tsx
@@ -1,6 +1,20 @@
 import BarsWrapper from "@/components/layouts/BarsWrapper";
+import { useData } from "@/hooks/useData";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 const SnippetsPage = () => {
+  const router = useRouter();
+  const { activeSnippetId } = useData();
+
+  useEffect(() => {
+    if (activeSnippetId) {
+      router.push({
+        pathname: "/snippets/[snippetId]",
+        query: { snippetId: activeSnippetId },
+      });
+    }
+  }, [activeSnippetId, router]);
   return <h1>Snippets Page</h1>;
 };
 
