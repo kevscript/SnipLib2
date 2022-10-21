860508a222c87dde944f214599c0f936422f8ab3
kevscript <kevscripted@gmail.com>
2022-10-21 19:40:47 +0200

    added auth menu

--- components/SideBar/Authbox.tsx
+++ components/SideBar/Authbox.tsx
@@ -2,9 +2,22 @@ import MoreIcon from "@/components/icons/More";
 import { useSession, signIn, signOut } from "next-auth/react";
 import Image from "next/image";
 import GithubIcon from "@/components/icons/Github";
+import { useRef, useState } from "react";
+import useOutsideClick from "@/hooks/useOutsideClick";
+import Link from "next/link";
+import SettingsIcon from "../icons/Settings";
+import LogoutIcon from "../icons/Logout";
 
 const Authbox = () => {
   const { data, status } = useSession();
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
+
+  const clickContainerRef = useRef(null);
+
+  useOutsideClick({
+    action: () => setMenuIsOpen(false),
+    ref: clickContainerRef,
+  });
 
   if (status === "loading") {
     return <h3>Loading...</h3>;
@@ -12,7 +25,10 @@ const Authbox = () => {
 
   if (data && data.user && status === "authenticated") {
     return (
-      <div className="flex items-center justify-between w-full pt-12 mt-auto flex-nowrap">
+      <div
+        className="flex items-center justify-between flex-shrink-0 w-full mt-auto flex-nowrap"
+        ref={clickContainerRef}
+      >
         <div className="flex flex-nowrap">
           <div className="relative w-10 h-10 overflow-hidden rounded-lg bg-marine">
             <Image src={data.user.image!} layout="fill" alt="user avatar" />
@@ -24,10 +40,33 @@ const Authbox = () => {
         </div>
         <div
           className="flex items-center justify-center w-6 h-full cursor-pointer group"
-          onClick={() => signOut()}
+          onClick={() => setMenuIsOpen((x) => !x)}
         >
-          <MoreIcon className="w-4 stroke-carbon-300 group-hover:stroke-white" />
+          <MoreIcon
+            className={`w-4 ${
+              menuIsOpen ? "stroke-white" : "stroke-carbon-300"
+            } group-hover:stroke-white `}
+          />
         </div>
+        {menuIsOpen && (
+          <div className="absolute w-40 overflow-hidden rounded drop-shadow-[0_1px_4px_#0e0e0e] bottom-8 left-[300px] bg-carbon-700 border border-carbon-400">
+            <ul className="flex flex-col w-full text-sm">
+              <Link href="/settings">
+                <li className="flex items-center w-full h-10 px-4 transition-all border-b cursor-pointer hover:bg-carbon-600 gap-x-2 border-carbon-400">
+                  <SettingsIcon className="w-3.5 h-3.5" />
+                  <span>Preferences</span>
+                </li>
+              </Link>
+              <li
+                className="flex items-center justify-between w-full h-10 px-4 transition-all cursor-pointer hover:bg-carbon-600"
+                onClick={() => signOut()}
+              >
+                <span>Sign out</span>
+                <LogoutIcon className="w-3.5 h-3.5" />
+              </li>
+            </ul>
+          </div>
+        )}
       </div>
     );
   }
---
+++ components/icons/Logout.tsx
@@ -0,0 +1,20 @@
+import { SvgIconProps } from "./types";
+
+const LogoutIcon = ({ className }: SvgIconProps) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      className={className}
+      viewBox="0 0 24 24"
+    >
+      <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4M16 17l5-5-5-5M21 12H9" />
+    </svg>
+  );
+};
+
+export default LogoutIcon;
