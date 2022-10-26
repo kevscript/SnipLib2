5f08d7811b7c021e87ba10cb099f332dbd3e9cb6
kevscript <kevscripted@gmail.com>
2022-10-26 18:58:46 +0200

    added Danger and Info icons

---
+++ components/icons/Info.tsx
@@ -0,0 +1,21 @@
+import { SvgIconProps } from "./types";
+
+const InfoIcon = ({ className }: SvgIconProps) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      className={className}
+      viewBox="0 0 24 24"
+    >
+      <circle cx="12" cy="12" r="10" />
+      <path d="M12 16v-4M12 8h.01" />
+    </svg>
+  );
+};
+
+export default InfoIcon;
