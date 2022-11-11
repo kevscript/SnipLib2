b7df7485a07c7a8319b7e66665d28871a95ca5d4
kevscript <kevscripted@gmail.com>
2022-11-11 15:44:33 +0100

    fix: integrated SnipItem to FavBar

--- components/bars/FavBar.tsx
+++ components/bars/FavBar.tsx
@@ -3,6 +3,7 @@ import { langList } from "@/utils/langList";
 import { motion } from "framer-motion";
 import Link from "next/link";
 import BarHeaderWrapper from "./BarHeaderWrapper";
+import SnipItem from "./SnipItem";
 
 type FavBarProps = {
   favSnippets: Snippet[] | undefined;
@@ -23,44 +24,13 @@ const FavBar = ({ favSnippets, activeSnippetId }: FavBarProps) => {
       {favSnippets && favSnippets.length > 0 && (
         <ul className="flex flex-col flex-1 overflow-y-auto">
           {favSnippets.map((snippet) => (
-            <Link
+            <SnipItem
               key={snippet._id.toString()}
-              href={{
-                pathname: `/favorites/[snippetId]`,
-                query: {
-                  snippetId: snippet._id.toString(),
-                },
-              }}
-              passHref
-            >
-              <li
-                className={`flex overflow-hidden flex-col w-full px-8 pt-6 pb-8 border-b-2 cursor-pointer border-carbon-600 group hover:bg-carbon-400 ${
-                  activeSnippetId === snippet._id.toString()
-                    ? "bg-carbon-400"
-                    : "bg-carbon-500"
-                }`}
-                key={snippet._id.toString()}
-              >
-                <span className="font-semibold">{snippet.title}</span>
-                <div className="flex mt-3 flex-nowrap">
-                  <span
-                    className={`text-xs capitalize`}
-                    style={{
-                      color:
-                        langList.find((l) => l.id === snippet.language)
-                          ?.color || "#FFF",
-                    }}
-                  >
-                    {snippet.language}
-                  </span>
-                  <ul className="flex ml-4 truncate flex-nowrap gap-x-2">
-                    <span className="text-xs text-carbon-300">
-                      {snippet.tags && snippet.tags.map((tag, i) => `#${tag} `)}
-                    </span>
-                  </ul>
-                </div>
-              </li>
-            </Link>
+              snippet={snippet}
+              isActive={activeSnippetId === snippet._id.toString()}
+              path={`/search/${snippet._id.toString()}`}
+              color={langList.find((l) => l.id === snippet.language)?.color}
+            />
           ))}
         </ul>
       )}
