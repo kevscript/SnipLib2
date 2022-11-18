e6efac17e9afd5def12add1ea3278e050a8c6a73
kevscript <kevscripted@gmail.com>
2022-11-18 19:24:47 +0100

    added Eye Icon

---
+++ components/icons/Eye.tsx
@@ -0,0 +1,41 @@
+import { SvgIconProps } from "./types";
+
+type EyeIconProps = SvgIconProps & {
+  off?: boolean;
+};
+
+const EyeIcon = ({ className, off }: EyeIconProps) => {
+  if (off) {
+    return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        className={className}
+        viewBox="0 0 24 24"
+      >
+        <path d="M17.94 17.94A10.07 10.07 0 0 1 12 20c-7 0-11-8-11-8a18.45 18.45 0 0 1 5.06-5.94M9.9 4.24A9.12 9.12 0 0 1 12 4c7 0 11 8 11 8a18.5 18.5 0 0 1-2.16 3.19m-6.72-1.07a3 3 0 1 1-4.24-4.24M1 1l22 22" />
+      </svg>
+    );
+  }
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      className={className}
+      viewBox="0 0 24 24"
+    >
+      <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z" />
+      <circle cx="12" cy="12" r="3" />
+    </svg>
+  );
+};
+
+export default EyeIcon;
