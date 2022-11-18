6d4889fd85d8f42dca89527fb76beea5b20eab55
kevscript <kevscripted@gmail.com>
2022-11-18 19:56:07 +0100

    added default UI structure to public snippet page

--- pages/snippet/[snippetId].tsx
+++ pages/snippet/[snippetId].tsx
@@ -1,10 +1,25 @@
+import BarsWrapper from "@/components/layouts/BarsWrapper";
+import { useCodeMirror } from "@/hooks/useCodeMirror";
+import { usePreferences } from "@/hooks/usePreferences";
+import Snippet from "@/models/Snippet";
 import { getPublicSnippet } from "@/utils/getPublicSnippet";
+import { langList, LanguageIds } from "@/utils/langList";
 import { useQuery } from "@tanstack/react-query";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 const PublicSnippetPage = () => {
   const router = useRouter();
   const { snippetId } = router.query;
+  const { preferences } = usePreferences();
+  const [snippet, setSnippet] = useState<Snippet | null>(null);
+
+  const { container } = useCodeMirror({
+    readOnly: true,
+    doc: snippet ? snippet.content : "",
+    preferences: preferences,
+    lang: (snippet?.language as LanguageIds) || "javascript",
+  });
 
   const { data, isSuccess, isError, isLoading } = useQuery(
     ["publicSnippet"],
@@ -16,6 +31,12 @@ const PublicSnippetPage = () => {
     }
   );
 
+  useEffect(() => {
+    if (data) {
+      setSnippet(data);
+    }
+  }, [data]);
+
   if (isLoading) {
     return <h1>Loading...</h1>;
   }
@@ -25,9 +46,43 @@ const PublicSnippetPage = () => {
   }
 
   return (
-    <div>
-      <h1>{snippetId}</h1>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
+    <div className="page">
+      {snippet && (
+        <div className="flex flex-col flex-1 w-full my-8 min-h-[480px] flex-shrink-0">
+          <h3 className="text-2xl font-bold">{snippet.title}</h3>
+          <p className="mt-4">{snippet.description}</p>
+
+          <div className="flex items-center justify-between w-full mt-8">
+            <div className="flex items-center justify-center px-4 py-1 text-sm rounded-sm bg-carbon-600">
+              <span className="text-sm">
+                {langList.find((l) => l.id === snippet.language)?.label}
+              </span>
+            </div>
+
+            <ul className="flex truncate flex-nowrap gap-x-2">
+              {snippet.tags &&
+                snippet.tags.map((tag, i) => (
+                  <li
+                    key={tag + i}
+                    className="flex items-center justify-center px-2 py-1 text-sm transition-all rounded-sm bg-carbon-600 hover:bg-carbon-400"
+                  >
+                    #{tag}
+                  </li>
+                ))}
+            </ul>
+          </div>
+
+          <div className="h-full mt-2 overflow-y-auto rounded">
+            <p className="sr-only">
+              The Editor uses Tab key to indent code. If you are focused on the
+              editor and want to keep navigating instead of indenting code :
+              press Escape, then Tab to move to the field after the editor. Or
+              Escape, Shift-Tab to move to the field before the editor.
+            </p>
+            <div ref={container} className="w-full h-full"></div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
