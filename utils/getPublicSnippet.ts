deab291d278ba84326d9bc8641dde603e0d5d9c9
kevscript <kevscripted@gmail.com>
2022-11-23 18:21:31 +0100

    created api/public/snippet route and updated app with it

--- utils/getPublicSnippet.ts
+++ utils/getPublicSnippet.ts
@@ -1,9 +1,11 @@
+import Snippet from "@/models/Snippet";
+
 export const getPublicSnippet = async ({
   snippetId,
 }: {
   snippetId: string;
-}): Promise<any> => {
-  const res = await fetch(`/api/snippet/${snippetId}`);
+}): Promise<{ snippet: Snippet }> => {
+  const res = await fetch(`/api/public/snippet/${snippetId}`);
   const data = await res.json();
   if (res.status === 200) {
     return data;
