50698913c27ca43f8d0cb53d8cc44d92b67c2fa5
kevscript <kevscripted@gmail.com>
2022-11-25 16:41:23 +0100

    animated and updated UI of PublicSnippetPage

--- pages/snippet/[snippetId].tsx
+++ pages/snippet/[snippetId].tsx
@@ -12,6 +12,7 @@ import { getPublicSnippet } from "@/utils/getPublicSnippet";
 import { langList, LanguageIds } from "@/utils/langList";
 import timeSince from "@/utils/timeSince";
 import { useQuery } from "@tanstack/react-query";
+import { motion } from "framer-motion";
 import { signIn, useSession } from "next-auth/react";
 import Head from "next/head";
 import Link from "next/link";
@@ -61,49 +62,43 @@ const PublicSnippetPage = () => {
     );
   }
 
-  if (!snippet) {
-    return (
-      <div className="flex items-center justify-center w-full h-screen">
-        <Head>
-          <title>Snippet - SnipLib</title>
-          <meta name="description" content="Loading current public snippet." />
-        </Head>
-        <Loader />
-      </div>
-    );
-  }
-
   return (
-    <div className="w-full h-screen overflow-auto">
-      <Toast ref={toastRef} />
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.25 }}
+      className="w-full h-screen overflow-auto"
+    >
       <Head>
-        <title>{snippet?.title} - Sniplib</title>
+        <title>{snippet && `${snippet.title} - SnipLib`}</title>
         <meta name="description" content="Public snippet" />
       </Head>
+      <Toast ref={toastRef} />
       <div className="w-[1440px] mx-auto max-w-[90%] pt-8 pb-16">
         <div className="flex justify-between flex-nowrap">
           <Link href="/">
-            <div className="flex items-center cursor-pointer gap-x-2">
-              <div className="w-8 h-8 rounded bg-marine-500"></div>
-              <span className="text-xl font-black">SnipLib</span>
+            <div>
+              <SnipLogo stage={2} />
             </div>
           </Link>
           <div className="flex items-center gap-x-4">
-            <IconButton
-              onClick={() => {
-                navigator.clipboard.writeText(snippet.content);
-                toastRef.current.showToast({
-                  type: "neutral",
-                  title: "Copied to clipboard",
-                  duration: 1500,
-                });
-              }}
-              icon={
-                <CopyIcon className="w-4 h-4 transition-all ease-out group-hover:stroke-marine-50" />
-              }
-              tooltipId="copy"
-              tooltipText="Copy snippet"
-            />
+            {snippet && (
+              <IconButton
+                onClick={() => {
+                  navigator.clipboard.writeText(snippet.content);
+                  toastRef.current.showToast({
+                    type: "neutral",
+                    title: "Copied to clipboard",
+                    duration: 1500,
+                  });
+                }}
+                icon={
+                  <CopyIcon className="w-4 h-4 transition-all ease-out group-hover:stroke-marine-50" />
+                }
+                tooltipId="copy"
+                tooltipText="Copy snippet"
+              />
+            )}
             {status === "unauthenticated" && (
               <Button
                 label="Sign in with GitHub"
@@ -113,49 +108,57 @@ const PublicSnippetPage = () => {
           </div>
         </div>
 
-        <div className="flex flex-col mt-16">
-          <h3 className="text-2xl font-bold">{snippet.title}</h3>
-          <p className="mt-4">{snippet.description}</p>
+        {!snippet && (
+          <div className="flex items-center justify-center w-full mt-16">
+            <Loader />
+          </div>
+        )}
 
-          <div className="flex items-center justify-between w-full mt-8">
-            <div className="flex items-center justify-center px-4 py-1 text-sm rounded-sm bg-carbon-500">
-              <span className="text-sm">
-                {langList.find((l) => l.id === snippet.language)?.label}
-              </span>
+        {snippet && (
+          <div className="flex flex-col mt-16">
+            <h3 className="text-2xl font-bold">{snippet.title}</h3>
+            <p className="mt-4">{snippet.description}</p>
+
+            <div className="flex items-center justify-between w-full mt-8">
+              <div className="flex items-center justify-center px-4 py-1 text-sm rounded-sm bg-carbon-500">
+                <span className="text-sm">
+                  {langList.find((l) => l.id === snippet.language)?.label}
+                </span>
+              </div>
+
+              <ul className="flex truncate flex-nowrap gap-x-2">
+                {snippet.tags &&
+                  snippet.tags.map((tag, i) => (
+                    <li
+                      key={tag + i}
+                      className="flex items-center justify-center px-2 py-1 text-sm rounded-sm bg-carbon-500"
+                    >
+                      #{tag}
+                    </li>
+                  ))}
+              </ul>
             </div>
 
-            <ul className="flex truncate flex-nowrap gap-x-2">
-              {snippet.tags &&
-                snippet.tags.map((tag, i) => (
-                  <li
-                    key={tag + i}
-                    className="flex items-center justify-center px-2 py-1 text-sm rounded-sm bg-carbon-500"
-                  >
-                    #{tag}
-                  </li>
-                ))}
-            </ul>
-          </div>
+            <div className="h-full mt-2 overflow-y-auto rounded">
+              <p className="sr-only">
+                The Editor uses Tab key to indent code. If you are focused on
+                the editor and want to keep navigating instead of indenting code
+                : press Escape, then Tab to move to the field after the editor.
+                Or Escape, Shift-Tab to move to the field before the editor.
+              </p>
+              <div ref={container} className="w-full h-full"></div>
+            </div>
 
-          <div className="h-full mt-2 overflow-y-auto rounded">
-            <p className="sr-only">
-              The Editor uses Tab key to indent code. If you are focused on the
-              editor and want to keep navigating instead of indenting code :
-              press Escape, then Tab to move to the field after the editor. Or
-              Escape, Shift-Tab to move to the field before the editor.
-            </p>
-            <div ref={container} className="w-full h-full"></div>
+            <div className="flex justify-between mt-2 text-xs text-carbon-300">
+              <span>created {timeSince(new Date(snippet.createdAt))}</span>
+              {snippet.updatedAt !== snippet.createdAt && (
+                <span>edited {timeSince(new Date(snippet.updatedAt))}</span>
+              )}
+            </div>
           </div>
-
-          <div className="flex justify-between mt-2 text-xs text-carbon-300">
-            <span>created {timeSince(new Date(snippet.createdAt))}</span>
-            {snippet.updatedAt !== snippet.createdAt && (
-              <span>edited {timeSince(new Date(snippet.updatedAt))}</span>
-            )}
-          </div>
-        </div>
+        )}
       </div>
-    </div>
+    </motion.div>
   );
 };
 
