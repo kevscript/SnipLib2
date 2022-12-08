6349add9c013119e712765b26eedb31fba8d31a7
kevscript <kevscripted@gmail.com>
2022-12-08 14:29:15 +0100

    added command for headless cypress component tests

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from "cypress";
 
 export default defineConfig({
-  "component": {
-    "devServer": {
-      "framework": "next",
-      "bundler": "webpack"
-    }
-  }
-})
+  component: {
+    devServer: {
+      framework: "next",
+      bundler: "webpack",
+    },
+    video: false,
+  },
+});
