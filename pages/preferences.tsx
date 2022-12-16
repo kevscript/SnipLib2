459eddca46613a7d0f7f3d99eb2b71fd98e232b9
kevscript <kevscripted@gmail.com>
2022-12-16 14:09:05 +0100

    created DeleteAccountWidget and integrated it to preferences page

--- pages/preferences.tsx
+++ pages/preferences.tsx
@@ -8,6 +8,8 @@ import { useCodeMirror } from "@/hooks/useCodeMirror";
 import { useRouter } from "next/router";
 import Button from "@/components/shared/Button";
 import Head from "next/head";
+import { useSession } from "next-auth/react";
+import DeleteAccountWidget from "@/components/widgets/DeleteAccountWidget";
 
 export type PreferencesFormErrors = {
   theme: string[];
@@ -17,16 +19,18 @@ export type PreferencesFormErrors = {
   snippetVisibility: string[];
 };
 
-const txt = `const files = [ 'foo.txt ', '.bar', '   ', 'baz.foo' ];
-const filePaths = files
-  .map(file => file.trim())
-  .filter(Boolean)
-  .map(fileName => \`~/cool_app/\${fileName}\`);
+const txt = (name: string) => `import { createRoot } from 'react-dom/client';
 
-// filePaths = [ '~/cool_app/foo.txt', '~/cool_app/.bar', '~/cool_app/baz.foo']`;
+function HelloMessage({ name }) {
+  return <div>Hello {name}</div>;
+}
+
+const root = createRoot(document.getElementById('container'));
+root.render(<HelloMessage name="${name}" />);`;
 
 const PreferencesPage = () => {
   const router = useRouter();
+  const { data: session } = useSession();
   const { preferences, updateLocalPreferences } = usePreferences();
   const [preferencesForm, setPreferencesForm] = useState<Preferences | null>(
     null
@@ -38,7 +42,9 @@ const PreferencesPage = () => {
   const { container } = useCodeMirror({
     preferences: preferencesForm || preferences,
     readOnly: false,
-    doc: txt.toString(),
+    doc: session?.user.name
+      ? txt(session.user.name).toString()
+      : txt("User").toString(),
   });
 
   useEffect(() => {
@@ -69,7 +75,7 @@ const PreferencesPage = () => {
   };
 
   return (
-    <div className="mt-8">
+    <div className="mt-8 mb-16">
       <Head>
         <title>User Preferences - Sniplib</title>
         <meta name="description" content="Change your editor & app settings" />
@@ -96,12 +102,12 @@ const PreferencesPage = () => {
         </div>
       </div>
 
-      {preferencesForm && formErrors && (
-        <>
-          <section className="w-full mt-12">
-            <h3 className="font-bold">User Settings</h3>
-            <div className="w-full h-[1px] bg-carbon-400 mt-2" />
-            <div className="flex flex-wrap gap-4 mt-8">
+      <>
+        <section className="w-full mt-12">
+          <h3 className="font-bold">User Settings</h3>
+          <div className="w-full h-[1px] bg-carbon-400 mt-2" />
+          <div className="flex flex-wrap gap-4 mt-8">
+            {preferencesForm && formErrors && (
               <FormSelect
                 label="Default snippet visibility"
                 name="snippetVisibility"
@@ -117,64 +123,81 @@ const PreferencesPage = () => {
                   public
                 </option>
               </FormSelect>
-            </div>
-          </section>
+            )}
+          </div>
+        </section>
 
-          <section className="w-full mt-12">
-            <h3 className="font-bold">Editor Settings</h3>
-            <div className="w-full h-[1px] bg-carbon-400 mt-2" />
+        <section className="w-full mt-12">
+          <h3 className="font-bold">Editor Settings</h3>
+          <div className="w-full h-[1px] bg-carbon-400 mt-2" />
 
-            <div className="flex flex-wrap gap-4 mt-8">
-              <FormSelect
-                label="Theme"
-                name="theme"
-                className="w-48"
-                value={preferencesForm.theme}
-                errors={formErrors.theme}
-                handleValue={(e) => handleFormValue(e, "theme")}
-              >
-                {getThemes().map((theme) => (
-                  <option key={theme.name} value={theme.name}>
-                    {theme.name}
-                  </option>
-                ))}
-              </FormSelect>
-              <FormSelect
-                label="Font"
-                name="font"
-                className="w-48"
-                value={preferencesForm.font}
-                errors={formErrors.font}
-                handleValue={(e) => handleFormValue(e, "font")}
-              >
-                <option>Inter</option>
-              </FormSelect>
-              <FormInputNumber
-                label="Font size (px)"
-                name="fontSize"
-                value={preferencesForm.fontSize}
-                errors={formErrors.fontSize}
-                className="w-32"
-                min={10}
-                max={32}
-                handleValue={(e) => handleFormValue(e, "fontSize")}
+          <div className="flex flex-wrap gap-4 mt-8">
+            {preferencesForm && formErrors && (
+              <>
+                <FormSelect
+                  label="Theme"
+                  name="theme"
+                  className="w-48"
+                  value={preferencesForm.theme}
+                  errors={formErrors.theme}
+                  handleValue={(e) => handleFormValue(e, "theme")}
+                >
+                  {getThemes().map((theme) => (
+                    <option key={theme.name} value={theme.name}>
+                      {theme.name}
+                    </option>
+                  ))}
+                </FormSelect>
+                <FormSelect
+                  label="Font"
+                  name="font"
+                  className="w-48"
+                  value={preferencesForm.font}
+                  errors={formErrors.font}
+                  handleValue={(e) => handleFormValue(e, "font")}
+                >
+                  <option>Inter</option>
+                </FormSelect>
+                <FormInputNumber
+                  label="Font size (px)"
+                  name="fontSize"
+                  value={preferencesForm.fontSize}
+                  errors={formErrors.fontSize}
+                  className="w-32"
+                  min={10}
+                  max={32}
+                  handleValue={(e) => handleFormValue(e, "fontSize")}
+                />
+                <FormSelect
+                  label="Tab spacing"
+                  name="tabSpacing"
+                  value={preferencesForm.tabSpacing}
+                  errors={formErrors.tabSpacing}
+                  handleValue={(e) => handleFormValue(e, "tabSpacing")}
+                >
+                  <option>2</option>
+                  <option>4</option>
+                </FormSelect>
+              </>
+            )}
+          </div>
+
+          <div ref={container} className="mt-8"></div>
+        </section>
+
+        <section className="w-full mt-12">
+          <h3 className="font-bold">Account Settings</h3>
+          <div className="w-full h-[1px] bg-carbon-400 mt-2" />
+          <div className="flex flex-col flex-wrap gap-4 mt-8">
+            {session && (
+              <DeleteAccountWidget
+                userName={session.user.name || ""}
+                userId={session.user.id || ""}
               />
-              <FormSelect
-                label="Tab spacing"
-                name="tabSpacing"
-                value={preferencesForm.tabSpacing}
-                errors={formErrors.tabSpacing}
-                handleValue={(e) => handleFormValue(e, "tabSpacing")}
-              >
-                <option>2</option>
-                <option>4</option>
-              </FormSelect>
-            </div>
-          </section>
-        </>
-      )}
-
-      <div ref={container} className="mt-8"></div>
+            )}
+          </div>
+        </section>
+      </>
     </div>
   );
 };
